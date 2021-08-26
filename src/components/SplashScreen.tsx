e355072705bbbc72b1a031dd6ab279bb7edac3d3
alfredo.salzillo <noctisbels@gmai.com>
2021-08-26 14:18:38 +0200

    fix changed logo color in SplashScreen.tsx

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -3,7 +3,13 @@ import Logo from '@icons/Logo';
 
 const SplashScreen = () => (
   <div className="splashscreen">
-    <Logo style={{ width: 50, height: 50 }} />
+    <Logo
+      style={{
+        width: 50,
+        height: 50,
+        fill: '#78909c'
+      }}
+    />
   </div>
 );
 
